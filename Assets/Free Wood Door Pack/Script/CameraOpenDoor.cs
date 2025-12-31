035381fd0bf9b491791b3da10bb80dbca5c3ce6a
Darianlime <darianli447@gmail.com>
2025-12-30 16:53:12 -1000

    initial commit

---
+++ Assets/Free Wood Door Pack/Script/CameraOpenDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraDoorScript
+{
+public class CameraOpenDoor : MonoBehaviour {
+	public float DistanceOpen=3;
+	public GameObject text;
+	Camera cam;
+	//private int interactMask;
+	// Use this for initialization
+	void Start () {
+		//interactMask = LayerMask.GetMask("XR Simulation");
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Debug.DrawRay(cam.transform.position,
+              cam.transform.forward * DistanceOpen,
+              Color.red);
+		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, DistanceOpen)) {
+			Debug.Log(hit.transform.name);
+			if (hit.transform.GetComponent<DoorScript.Door>()) {
+				text.SetActive(true);
+				if (Input.GetKeyDown(KeyCode.R)) 
+					hit.transform.GetComponent<DoorScript.Door>().OpenDoor();
+			} else {
+				text.SetActive(false);
+			}
+		} else {
+			text.SetActive(false);
+			Debug.Log("no hit");
+		} 
+	}
+}
+}
